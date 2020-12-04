354238f609bc401081c4d2cfac6d99da5347883c
youshamahmood <sirprocrastinatealot@gmail.com>
2020-12-05 02:01:37 +0600

    test-deploy

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,10 @@ class Stock < ApplicationRecord
             secret_token: 'Tpk_fcdb52dda2654f9a821c43990487458a',
             endpoint: 'https://sandbox.iexapis.com/v1'
         )
-        client.price(ticker_symbol)
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end
     end 
 end
